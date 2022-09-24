98987b51537dddc4efa776c3251906725dadc034
Md Sazzad Hossain Salim <sazzad362@gmail.com>
2022-09-24 18:13:03 +0600

    just init

---
+++ assets/js/app.js
@@ -0,0 +1,17 @@
+// IIFE with jQuery Wrapper
+(function($) {
+  'use strict';
+
+  /*
+   *----------------------------------
+   * Document Ready
+   *----------------------------------
+   */
+	$(document).ready(function() {
+
+   
+
+	});// DOM Ready
+
+}(jQuery)); // IIFE
+
